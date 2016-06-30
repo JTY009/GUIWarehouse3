3a1d3f1d7b821874e83758f0ffb4ee5452bde15c
JTY009 <paultan38@gmail.com>
2016-06-30 10:11:43 +0000

    Reset Commit

--- src/main/scala/InventoryItem.scala
+++ src/main/scala/InventoryItem.scala
@@ -4,8 +4,10 @@
 /**
   * Created by MacZ on 17/06/2016.
   */
-case class InventoryItem (ID : Int, Name : String, Price:Double, Quantity : Int, QuantityPorus: Int, Location : Double) {
+case class InventoryItem (id : Int, item : String,  var quantity : Int, location : Double) {
 
-
-
+  //  val id = ID
+  //  val item = Item
+  //  var quantity = Quantity
 }
+
